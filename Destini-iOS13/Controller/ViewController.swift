53afca85b75a743d5ede6abbf98b805429212d5b
keoki33 <gwong323@yahoo.com>
2021-03-26 14:17:49 +1100

    lesson 111 step 1 completed

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
+    @IBOutlet var storyLabel: UILabel!
+    @IBOutlet var choice1Button: UIButton!
+    @IBOutlet var choice2Button: UIButton!
+
+    let story0 = "You see a fork in the road"
+    let choice1 = "Take a left."
+    let choice2 = "Take a right."
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        storyLabel.text = story0
     }
 
-
+    @IBAction func choiceMade(_ sender: UIButton) {}
 }
-
